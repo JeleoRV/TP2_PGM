9edc5a890644b6610b9a1f188356439c9c24a44a
JeromeEtLeo <leo.malbrand@eleves.ec-nantes.fr>
2017-11-23 09:29:18 +0100

    Fichiers pour la classe PGM_image

---
+++ PGM/PGM_image.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   PGM_image.cpp
+ * Author: GlouglouLand
+ * 
+ * Created on 23 novembre 2017, 09:23
+ */
+
+#include "PGM_image.h"
+
+PGM_image::PGM_image() {
+}
+
+PGM_image::PGM_image(const PGM_image& orig) {
+}
+
+PGM_image::~PGM_image() {
+}
+
