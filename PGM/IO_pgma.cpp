654d05ffc0d8704144cf3531853d69dec661a789
PierreLouis-Antonsanti <piloa08@gmail.com>
2017-11-23 09:06:54 +0100

    IO cpp

---
+++ PGM/IO_pgma.cpp
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:  IO_pgma.cpp
+ * Author: wall-e pour GlouglouLand
+ *
+ * Created on 23 novembre 2017, 08:32
+ * 
+ * To open .pgm file and write ASCII
+ */
+
