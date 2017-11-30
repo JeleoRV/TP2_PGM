441f47a09ddf60e09c290fffcb7a58ffb565ceb6
JeromeEtLeo <leo.malbrand@eleves.ec-nantes.fr>
2017-11-30 10:03:44 +0100

    Test

---
+++ PGM_VS/PGM_VS/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: GlouglouLand
+ *
+ * Created on 23 novembre 2017, 08:32
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "PGM_image.h"
+using namespace std;
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+
+    PGM_image lena("lena.pgm");
+    lena.seuillage(60);
+    lena.write("lena_seuillee.pgm");
+    cout << "Appuyer sur n'importe quelle touche puis ENTREE pour terminer l'application..." << endl;
+    string i;
+    cin >> i;
+    return 0;
+}
+
